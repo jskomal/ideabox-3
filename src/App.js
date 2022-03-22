3c2716e0c990955b4db0e2821e78bbdcd37e2641
Jordan Skomal <jskomal@gmail.com>
2022-03-22 09:20:19 -0600

    Add required files and import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react'
 
-export class App extends Component {
+import Logo from './modules/Logo'
+import Sidebar from './modules/Sidebar'
+import Ideas from './modules/Ideas'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ideas: [
+        {
+          id: 1,
+          title: 'Prank Travis',
+          description: 'Stick googly eyes on all his stuff'
+        },
+        {
+          id: 2,
+          title: 'Make a secret password app',
+          description:
+            'So you and your rideshare driver can both know neither one of you is lying'
+        },
+        {
+          id: 3,
+          title: 'Learn a martial art',
+          description: 'To exact vengeance upon my enemies'
+        }
+      ]
+    }
+  }
+
   render() {
-    return <div>App</div>
+    return (
+      <div>
+        <Logo />
+        <Sidebar />
+        <Ideas />
+      </div>
+    )
   }
 }
 
